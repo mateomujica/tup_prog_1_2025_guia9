43457e092cfc60044d7c61c46e85640a28f7c104
mateomujica <mateomujicam@gmail.com>
2025-06-08 21:31:46 -0300

    subida

---
+++ Ejercicio 1 Fibonacci Iterativo/Ejercicio 1 Fibonacci Iterativo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anterior = 0;
+            int actual = 1;
+
+            Console.WriteLine("Ingrese cantidad de terminos");
+            int terminos = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < terminos; i++)
+            {
+                Console.WriteLine("=" + actual);
+
+                int siguiente = anterior + actual;
+
+                anterior = actual;
+
+                actual = siguiente;
+            }
+        }
+    }
+}
---
+++ Ejercicio 1 Fibonacci Recursivo/Ejercicio 1 Fibonacci Recursivo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1_Fibonacci_Recursivo
+{
+    internal class Program
+    {
+        static int Metodo(int termino)
+        {
+            if (termino == 0 || termino == 1)
+            {
+                return termino;
+            }
+            else
+            {
+                return Metodo(termino - 1) + Metodo(termino-2);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de términos a mostrar: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Resultado");
+            for (int i=0;i<cantidad;i++)
+            {
+                Console.WriteLine(""+Metodo(i));
+            }
+        }
+    }
+}
